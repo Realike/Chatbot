73e4305adb716e5248766c642cd2588d6ef31549
dwx <861579607@qq.com>
2019-03-07 01:57:53 +0800

    update

--- data_utls.py
+++ data_utls.py
@@ -3,6 +3,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from vocabulary import PAD_token, SOS_token, EOS_token, UNK_token, Voc
+
 import csv
 import random
 import re
@@ -12,6 +14,8 @@ import codecs
 from io import open
 import itertools
 import math
+import pickle
+
 
 corpus_name = 'cornell movie-dialogs corpus'
 corpus = os.path.join('data', corpus_name)
@@ -110,53 +114,110 @@ with open(datafile, 'w', encoding='utf-8') as o:
 # print('\nSample lines from file:')
 # printLines(datafile)
 
-# Default word tokens ?UNK_token
-PAD_token = 0   # padding token
-SOS_token = 1   # start token
-EOS_token = 2   # end token
 
-class Voc:
-    def __init__(self, name):
-        self.name = name
-        self.trimmed = False
-        self.word2index = {}
-        self.word2count = {}
-        self.index2word = {PAD_token: 'PAD', SOS_token: 'SOS', EOS_token: 'EOS'}
-        self.num_words = 3  # Count SOS, EOS, PAD
+MAX_LENGTH =10  # Maximun sentence length to consider
 
-    def addSentence(self, sentence):
-        for word in sentence.split(' '):
-            self.addWord(word)
+# Turn a Unicode string to plain ASCII, thanks to
+# https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
 
-    def addWord(self, word):
-        if word not in self.word2index:
-            self.word2index[word] = self.num_words
-            self.word2count[word] = 1
-            self.num_words += 1
-        else:
-            self.word2count[word] += 1
+# Lowercase, trim, and remove non-letter characters
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r'([.!?])', r' \1', s)   # 标点前增加空格
+    s = re.sub(r'[^a-zA-Z.!?]+', r' ', s)   # 字母和标点之外变成空格
+    s = re.sub(r'\s+', r' ', s).strip()    # 由上可能导致多个空格，把多个空格变为一个空格并去首尾空格
 
-    # Remove words below a certain count threshold
-    def trim(self, min_count):
-        if self.trimmed:
-            return
-        self.Trimmed =True
+    return s
 
-        keep_words = []
+# Read query/response pairs and return a voc object
+def readVocs(datafile, corpus_name):
+    print('Reading lines...')
+    lines = open(datafile, encoding='utf-8').read().strip().split('\n')
+    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+    voc = Voc(corpus_name)
 
-        for k, v in self.word2count.items():
-            if v >= min_count:
-                keep_words.append(k)
-
-        print('keep_words {} / {} = {:.4f}'.format(len(keep_words),
-            len(self.word2index), len(keep_words) / len(self.word2index)))
+    return voc, pairs
 
 
-        # Reinitialize dictionaries
-        self.word2index = {}
-        self.word2count = {}
-        self.index2word = {PAD_token: "PAD", SOS_token: "SOS", EOS_token: "EOS"}
-        self.num_words = 3 # Count default tokens
+# Returns True iff both sentences in a pair 'p' are under the MAX_LENGTH threshold
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and len(p[1].split(' ')) <MAX_LENGTH
 
-        for word in keep_words:
-            self.addWord(word)
+
+
+# Filter pairs using filterPair condition
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+# Using the functions defined above, return a populated voc object and pairs list
+def loadPrepareData(corpus, corpus_name, datafile, save_dir):
+    print('\nStart preparing training data...')
+    voc, pairs = readVocs(datafile, corpus_name)
+    print('Read {!s} sentence pairs'.format(len(pairs)))
+    pairs = filterPairs(pairs)
+    print('Trimmed to {!s} sentence pairs'.format(len(pairs)))
+    print('Counting words...')
+    for pair in pairs:
+        voc.addSentence(pair[0])
+        voc.addSentence(pair[1])
+    print('Counted words:', voc.num_words)
+    return voc, pairs
+
+
+# Load/Assemble voc and qa_pairs
+save_dir = os.path.join('data', 'save')
+voc, pairs = loadPrepareData(corpus, corpus_name, datafile, save_dir)
+# Print some pairs to validate
+print('\npairs:')
+for pair in pairs[:10]:
+    print(pair)
+print('\n')
+
+MIN_COUNT = 3   # Minimum word count threshold for trimming
+
+def trimRareWords(voc, pairs, MIN_COUNT):
+    # Trim words used under the MIN_COUNT from the voc
+    voc.trim(MIN_COUNT)
+    # Filter out pairs with trimmed words
+    keep_pairs = []
+    for pair in pairs:
+        input_sentence = pair[0]
+        output_sentence = pair[1]
+        keep_input = True
+        keep_output = True
+        # Check input sentence
+        for word in input_sentence.split(' '):
+            if word not in voc.word2index:
+                keep_input = False
+                break
+        # Check output sentence
+        for word in output_sentence.split(' '):
+            if word not in voc.word2index:
+                keep_output = False
+                break
+
+        # Only keep pairs that do not contain trimmed word(s) in their input or output sentence
+        if keep_input and keep_output:
+            keep_pairs.append(pair)
+
+    print("Trimmed from {} pairs to {}, {:.4f} of total".format(len(pairs), len(keep_pairs), len(keep_pairs) / len(pairs)))
+    return keep_pairs
+
+# Trim voc and pairs
+pairs = trimRareWords(voc, pairs, MIN_COUNT)
+
+#
+# store voc and pairs
+print('Storing voc and pairs')
+file_voc = open('voc.pkl', 'wb')
+file_pairs = open('pairs.pkl', 'wb')
+pickle.dump(voc, file_voc)
+pickle.dump(pairs, file_pairs)
+file_voc.close()
+file_pairs.close()
+print('Storing compeletely')
---
+++ model.py
@@ -0,0 +1,109 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from vocabulary import PAD_token, SOS_token, EOS_token, UNK_token, Voc
+
+import torch
+from torch.jit import script, trace
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import csv
+import random
+import re
+import os
+import unicodedata
+import codecs
+from io import open
+import itertools
+import math
+import pickle
+
+USE_CUDA = torch.cuda.is_available()
+device = torch.device('cuda' if USE_CUDA else 'cpu')
+
+
+#
+# # load voc and pairs
+def loadDataset():
+    with open('voc.pkl', 'rb') as handle_voc:
+        voc = pickle.load(handle_voc)
+
+    with open('pairs.pkl', 'rb') as handle_pairs:
+        pairs = pickle.load(handle_pairs)
+
+    return voc, pairs
+
+voc, pairs = loadDataset()
+
+#
+# # add EOS_token
+def indexesFromSentence(voc, sentence):
+    return [voc.word2index[word] for word in sentence.split(' ')] + [EOS_token]
+
+#
+# 通过zip_longest()，zeroPadding时矩阵转置
+def zeroPadding(l, fillvalue=PAD_token):
+    return list(itertools.zip_longest(*l, fillvalue=fillvalue))
+
+#
+# mask矩阵
+def binaryMatrix(l, value=PAD_token):
+    m = []
+    for i, seq in enumerate(l):
+        m.append([])
+        for token in seq:
+            if token == PAD_token:
+                m[i].append(0)
+            else:
+                m[i].append(1)
+
+    return m
+
+#
+# Returns padded input sequence tensor and lengths
+def inputVar(l, voc):
+    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]  # word2index batch
+    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+    padList = zeroPadding(indexes_batch)
+    padVar = torch.longTensor(padList)  # list to longTensor,shape(max_length, batch_size)
+
+    return padVar, lengths
+
+#
+# # Returns padded target sequence tensor, padding mask, and max target length
+def outputVar(l, voc):
+    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
+    max_target_len = max([len(indexes) for indexes in indexes_batch])
+    padList = zeroPadding(indexes_batch)
+    mask = binaryMatrix(padList)
+    padVar = torch.longTensor(padList)
+
+    return padVar, mask, max_target_len
+
+#
+# # Returns all items for a given batch of pairs
+def batch2TrainData(voc, pair_batch):
+    pair_batch.sort(key=lambda x: len(x[0].split(' ')), reverse=True)
+    input_batch, output_batch = [], []
+    for pair in pair_batch:
+        input_batch.append(pair[0])
+        output_batch.append(pair[1])
+    inp, lengths = inputVar(input_batch, voc)
+    output, mask, max_target_len = outputVar(output_batch, voc)
+
+    return inp, lengths, output, mask, max_target_len
+
+#
+# Example for validation
+small_batch_size = 5
+batches = batch2TrainData(voc, [random.choice(pairs) for _ in range(small_batch_size)])
+input_variable, lengths, target_variable, mask, max_target_len = batches
+
+print('input_variable:', input_variable)
+print('length:', length)
+print('target_variable:' ,target_variable)
+print('mask:', mask)
+print('max_target_len:', max_target_len)
