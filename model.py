4b27cf4dbe2a9f75021c24168e7d195f8d86dd6e
root <861579607@qq.com>
2019-03-11 13:30:17 +0800

    update

--- model.py
+++ model.py
@@ -117,3 +117,51 @@ print('length:', lengths)
 print('target_variable:', target_variable)
 print('mask:', mask)
 print('max_target_len:', max_target_len)
+
+
+#
+# # EncoderRNN
+class EncoderRNN(nn.module):
+    def __init__(self, hidden_size, embedding, n_layers=1, dropout=0):
+        super(EncoderRNN, self).__init__()
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+        self.embedding = embedding
+        # input and hidden is hidden_size
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers,
+            dropout=(0 if n_layers == 1 else dropout), bidirectional= True)
+
+
+    def forward(self, input_seq, input_lengths, hidden=None):
+        # input_seq shape(max_length, batch) -> embedding shape(max_length, batch, hidden_size)
+        embedded = self.embedding(input_seq)
+        # Pack padded batch of sequences for RNN module
+        # 因为RNN(GRU)需要知道实际的长度，所以PyTorch提供了一个函数pack_padded_sequence把输入向量和长度pack
+        # 到一个对象PackedSequence里，这样便于使用。
+        packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
+
+        # 通过GRU进行forward计算，需要传入输入和隐变量
+        # 如果传入的输入是一个Tensor (max_length, batch, hidden_size)
+        # 那么输出outputs是(max_length, batch, hidden_size*num_directions)。
+        # 第三维是hidden_size和num_directions的混合，它们实际排列顺序是num_directions在前面，因此我们可以
+        # 使用outputs.view(seq_len, batch, num_directions, hidden_size)得到4维的向量。
+        # 其中第三维是方向，第四位是隐状态。
+
+        # 而如果输入是PackedSequence对象，那么输出outputs也是一个PackedSequence对象，我们需要用
+        # 函数pad_packed_sequence把它变成一个shape为(max_length, batch, hidden*num_directions)的向量以及
+        # 一个list，表示输出的长度，当然这个list和输入的input_lengths完全一样，因此通常我们不需要它。
+        outputs, hidden = self.gru(packed, hidden)
+
+        # 参考前面的注释，我们得到outputs为(max_length, batch, hidden*num_directions)
+        outputs, _ = torch.nn.utils.rnn.pad_packed_sequene(outputs)
+        # 我们需要把输出的num_directions双向的向量加起来
+        # 因为outputs的第三维是先放前向的hidden_size个结果，然后再放后向的hidden_size个结果
+        # 所以outputs[:, :, :self.hidden_size]得到前向的结果
+        # outputs[:, :, self.hidden_size:]是后向的结果
+        # 注意，如果bidirectional是False，则outputs第三维的大小就是hidden_size，
+        # 这时outputs[:, : ,self.hidden_size:]是不存在的，因此也不会加上去。
+
+        outputs = outputs[:, :, :self.hidden_size] + outputs[:, :, self.hidden_size:]
+
+        # 返回最终的输出和最后时刻的隐状态。
+        return outputs, hidden
