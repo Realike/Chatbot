0accfe9f9b75a2b79b501884ebc0fee7d490d1d9
root <861579607@qq.com>
2019-03-14 00:12:16 +0800

    update

--- model.py
+++ model.py
@@ -165,3 +165,51 @@ class EncoderRNN(nn.Module):
 
         # 返回最终的输出和最后时刻的隐状态。
         return outputs, hidden
+
+
+#
+# # Luong attention layer
+class Attn(torch.nn.Module):
+    def __init__(self, method, hidden_size):
+        super(Attn, self).__init__
+        self.method = method
+        if self.method on in ['dot', 'general', 'concat']:
+            raise ValueError(self.method, 'is not an appropriate attention method.')
+        self.hidden_size = hidden_size
+        if self.method == 'general':
+            self.attn = torch.nn.Linear(self.hidden_size, hidden_size)
+        elif self.method == 'concat':
+            self.attn = torch.nn.Linear(self.hidden_size * 2, hidden_size)
+            self.v = torch.nn.Parameter(torch.FloatTensor(hidden_size))
+
+    def dot_score(self, hidden, encoder_output):
+        # 输入hidden的shape是(1, batch=64, hidden_size=500)
+        # encoder_outputs的shape是(input_lengths=10, batch=64, hidden_size=500)
+        # hidden * encoder_output得到的shape是(10, 64, 500)，然后对第3维求和就可以计算出score。
+        return torch.sum(hidden * encoder_output, dim=2)
+
+    def general_score(self, hidden, encoder_output):
+        energy = self.atten(encoder_output)
+        return torch.sum(hidden * energy, dim=2)
+
+    def concat_score(self, hidden, encoder_output):
+        energy = self.attn(torch.cat((hidden.expand(encoder_output.size(0), -1, -1), encoder_output), 2)).tanh()
+        return torch.sum(self.v * energy, dim=2)
+
+    # 输入是上一个时刻的隐状态hidden和所有时刻的Encoder的输出encoder_outputs
+    # 输出是注意力的概率，也就是长度为input_lengths的向量，它的和加起来是1。
+    def forward(self, hidden, encoder_outputs):
+        if self.method == 'general':
+            attn_energies = self.general_score(hidden, encoder_outputs)
+        else if self.method == 'concat':
+            attn_energies = self.concat_score(hidden, encoder_outputs)
+        else if self.method == 'dot':
+            attn_energies = self.dot_score(hidden, encoder_outputs)
+
+        # Transpose max_length and batch_size dimensions
+        # 把attn_energies从(max_length=10, batch=64)转置成(64, 10)
+        attn_energies = attn_energies.t()
+
+        # 使用softmax函数把score变成概率，shape仍然是(64, 10)，然后用unsqueeze(1)变成
+        # (64, 1, 10)
+        return F.softmax(attn_energies, dim=1).unsqueeze(1)
